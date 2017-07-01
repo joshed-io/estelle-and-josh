4bd19844180b5c242b85b3c7435633ba0566f53f
Josh Dzielak <jdzielak@gmail.com>
2017-07-01 09:42:51 -0700

    Animate the cones and syc with the confetti

--- src/js/app.js
+++ src/js/app.js
@@ -6,14 +6,22 @@ $(document).ready(function() {
   $(".carousel").slick({
   });
 
-  $(".get-married").animate({
+  $(".get-married h1").animate({
     marginLeft: "0px"
   }, {
     duration: 1500,
     done: function() {
-      setTimeout(() => {
-        window.confettify();
-      }, 500);
+      $(".i-left, .i-right").css({
+        visibility: "visible"
+      });
+      $(".i-right, .i-left").animate({
+        left: 0
+      }, {
+        duration: 1000,
+        done: function() {
+          window.confettify();
+        }
+      });
     }
   });
 });
