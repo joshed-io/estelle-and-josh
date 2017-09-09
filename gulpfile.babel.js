ce3fb8a93f2fef062979771552e21397f7590420
Josh Dzielak <jdzielak@gmail.com>
2017-09-09 13:12:35 -0700

    Remove ./ to watch for new/deleted files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -50,6 +50,7 @@ gulp.task("server", ["hugo", "css", "js"], () => {
   });
   gulp.watch("./src/js/**/*.js", ["js"]);
   gulp.watch("./src/css/**/*.css", ["css"]);
+  gulp.watch("staging/*", ["convert"]);
   gulp.watch("./site/**/*", ["hugo"]);
 });
 
