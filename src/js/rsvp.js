b6065e6f8cbccb7f2e6d6876baa515078aa12f7f
Josh Dzielak <jdzielak@gmail.com>
2018-02-15 13:15:18 -0800

    Add a yes / no to the RSVP form
    Center the RSVP form

--- src/js/rsvp.js
+++ src/js/rsvp.js
@@ -1,24 +1,30 @@
 $("#rsvp-form").on("submit", function(event) {
-  console.log("here");
-  var youName = $("input[name='you_name']")[0].value;
-  var guestName = $("input[name='guest_name']")[0].value;
-  var foodAllergies = $("input[name='food_allergies']")[0].value;
-  if (!(youName)) {
-    alert('Please fill out your name in the form!');
+  event.preventDefault();
+
+  var rsvp = $("input[name='rsvp']:checked").val();
+  var youName = $("input[name='you_name']").val();
+  var guestName = $("input[name='guest_name']").val();
+  var foodAllergies = $("input[name='food_allergies']").val()
+  if (!(youName && rsvp)) {
+    alert('Please fill out your RSVP and your name in the form!');
     return;
   }
 
-  event.preventDefault();
   $.ajax({
     url: "https://hooks.zapier.com/hooks/catch/880865/z60o8j/",
     method: "POST",
     data: {
+      rsvp: rsvp,
       you_name: youName,
       guest_name: guestName,
       food_allergies: foodAllergies
     },
     success: function() {
-      document.location.href = "/rsvp-confirmed";
+      if (rsvp === "Yes") {
+        document.location.href = "/rsvp-confirmed";
+      } else {
+        document.location.href = "/rsvp-no";
+      }
     }
   });
 });
